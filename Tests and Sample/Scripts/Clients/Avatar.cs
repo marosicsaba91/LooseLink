2f0be6daa39efd9f92edd07ea7f242be65a6a3fc
Marosi Csaba <marosicsaba91@gmail.com>
2021-09-20 01:20:53 +0200

    Sample Update

---
+++ Tests and Sample/Scripts/Clients/Avatar.cs
@@ -0,0 +1,26 @@
+﻿using MUtility;
+using UnityEngine;
+using UnityServiceLocator;
+
+public class Avatar : MonoBehaviour
+{
+    [SerializeField] float velocity = 1;
+    IAvatarInputProvider _input;
+
+    void Awake()
+    {
+        UpdateInputProvider();
+        ServiceLocator.Environment.SubscribeToEnvironmentChange<IAvatarInputProvider>(UpdateInputProvider);
+    }
+    
+    void UpdateInputProvider()
+    {
+        _input = ServiceLocator.Get<IAvatarInputProvider>();
+    }
+
+    void Update()
+    {
+        if (_input.TryGetDirection(out Direction2D direction))
+            transform.localPosition += (Vector3)direction.ToVector().normalized * (Time.deltaTime * velocity);
+    }
+}
---
+++ Tests and Sample/Scripts/Managers/KeyboardInputProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MUtility;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+[CreateAssetMenu]
+public class KeyboardInputProvider : ScriptableObject, IAvatarInputProvider
+{
+    [SerializeField] KeyCode up = KeyCode.UpArrow;
+    [SerializeField] KeyCode down = KeyCode.DownArrow;
+    [SerializeField] KeyCode left = KeyCode.LeftArrow;
+    [SerializeField] KeyCode right = KeyCode.RightArrow;
+    
+    public Object UnityObject => this;
+    public string NameOfDirectionCommand(GeneralDirection2D direction) => 
+        DirectionToKeyCode(direction).ToString();
+
+    public bool IsDirectionCommandPressed(GeneralDirection2D direction) =>
+        Input.GetKey(DirectionToKeyCode(direction));
+
+    KeyCode DirectionToKeyCode(GeneralDirection2D direction)
+    {
+        switch (direction)
+        {
+            case GeneralDirection2D.Up: return up;
+            case GeneralDirection2D.Down: return down;
+            case GeneralDirection2D.Right: return right;
+            case GeneralDirection2D.Left: return left;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+        }
+    }
+}
